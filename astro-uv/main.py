59fe9a101eb705f95bad1629ea33576ce925a5b5
kaelgsson <mikael.s.gustafsson@proton.me>
2025-12-28 13:10:15 +0100

    12 of 19

--- astro-uv/main.py
+++ astro-uv/main.py
@@ -12,6 +12,11 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
@@ -22,8 +27,12 @@ def main():
                 return
 
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+
+        updatable.update(dt)
+
+        for item in drawable:
+            item.draw(screen)
+
         pygame.display.flip()
         
         dt = clock.tick(60) / 1000
