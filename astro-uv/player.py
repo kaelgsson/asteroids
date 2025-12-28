5c3dad7e2685a855bef61e00c0859ddbd822c77e
kaelgsson <mikael.s.gustafsson@proton.me>
2025-12-28 12:44:56 +0100

    10 of 19

--- astro-uv/player.py
+++ astro-uv/player.py
@@ -1,12 +1,12 @@
 import pygame
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, LINE_WIDTH
+from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED
 
 
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, radius=PLAYER_RADIUS)
-        self.rotation = 0
+        self.rotation: int = 0
 
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -18,3 +18,14 @@ class Player(CircleShape):
 
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), width=LINE_WIDTH)
+
+    def rotate(self, dt: int):
+        self.rotation += PLAYER_TURN_SPEED * dt
+
+    def update(self, dt: int):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
